e11f2614dabe0f62f426c1e0ddc5a2d56c5910d1
Sylar Ken <40093634@qq.com>
2021-06-16 22:48:04 +0800

    Initial commit

---
+++ bao_test.py
@@ -0,0 +1,19 @@
+import baostock as bs
+import pandas as pd
+
+lg = bs.login()
+print('login respond error_code:'+lg.error_code)
+print('login respond  error_msg:'+lg.error_msg)
+
+rs = bs.query_all_stock(day="2021-06-15")
+print('query_all_stock respond error_code:'+rs.error_code)
+print('query_all_stock respond  error_msg:'+rs.error_msg)
+
+data_list = []
+while (rs.error_code == '0') & rs.next():
+    data_list.append(rs.get_row_data())
+result = pd.DataFrame(data_list, columns=rs.fields)
+
+csv = result.to_csv("all_stock.csv", encoding="gbk", index=False)
+print(result)
+bs.logout()
